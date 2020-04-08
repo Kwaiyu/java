42ed2cfd280a869c617b0a8874df45fdd6f4e8e8
lsaiah <lsaiah@126.com>
2020-04-09 00:13:19 +0800

    0409

---
+++ src/main/java/com/lsaiah/java/StackDemo.java
@@ -0,0 +1,35 @@
+package com.lsaiah.java;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class StackDemo {
+    public static void main(String[] args) {
+        String hex = toHex(12500);
+        if (hex.equalsIgnoreCase("30D4")){
+            System.out.println("测试通过");
+        }else {
+            System.out.println("测试失败");
+        }
+    }
+
+    static String toHex(int n) {
+        //创建一个空栈，通过Deque实现
+        Deque<Character> dq = new ArrayDeque<>();
+        //循环计算12500%16=0压入栈
+        for (int i=n; i !=0; i= i/16){
+            int remainder = i%16;
+            int ch;
+            if (remainder >= 10){
+                ch = 65 + (remainder-10);
+            }else {
+                ch = remainder + 48;
+            }
+            dq.push(Character.valueOf((char) ch));
+        }
+        StringBuilder result = new StringBuilder(dq.size());
+        while (dq.peek() != null){
+            result.append(dq.pop());
+        }
+        return result.toString();
+    }
+}
